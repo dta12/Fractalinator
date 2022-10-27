635b49a463146a55cd4d18ef4719d3792570d59d
zaf22 <zaf22@pitt.edu>
2022-10-27 11:59:04 -0400

    implemented database for storing account info

---
+++ database.py
@@ -0,0 +1,66 @@
+import datetime
+from google.cloud import datastore
+
+class FractalData():
+
+    def __init__(self):
+        """Initialize the database"""
+        self.client = datastore.Client()
+
+    def add_user(self, user, password):
+        """Add username and password of account to database"""
+        query = self.client.query(kind='accounts')
+        query.add_filter("username", "=", user)
+        if (len(list(query.fetch())) != 0):
+            return False
+
+        key = self.client.key("accounts")
+        account = datastore.Entity(key)
+
+        account.update(
+            {
+                "username": user,
+                "password": password
+            }
+        )
+        self.client.put(account)
+        return True
+
+    def credentials(self, username, password):
+        """Verifies the username and password are correct"""
+        query = self.client.query(kind='accounts')
+        query.add_filter("username", "=", username)
+        for account in query.fetch():
+            if (account['password'] == password):
+                return True
+            return False
+
+    def getID(self, username):
+        """Gets the ID of the user, a loophole for rerouting"""
+        query = self.client.query(kind='accounts')
+        query.add_filter("username", "=", username)
+        
+        for entity in query.fetch():
+            return str(entity.key)  
+
+    def add_fractal(self, userID, length, angle, branchwidth):
+        """Add fractal to fractal database using userID """
+        key = self.client.key("fractals")
+        fractal = datastore.Entity(key)
+
+        fractal.update(
+            {
+                "userID": userID,
+                "length": length,
+                "angle": angle,
+                "branchWidth": branchwidth
+            }
+        )
+        self.client.put(fractal)
+
+    def get_fractals(self, userID):
+        """Grabs all the fractal info from a given account"""
+        query = self.client.query(kind='fractals')
+        query.add_filter("userID", "=", userID)
+
+        return list(query.fetch())
--- main.py
+++ main.py
@@ -1,25 +1,51 @@
+from fileinput import filename
 import flask
-
+from flask import url_for
+import database
 
 app = flask.Flask(__name__)
-
+f = database.FractalData()
 
 # note that we can include several routes for the same function.
+
+
 @app.route('/')
-@app.route('/index.html')
-@app.route('/index')
+@app.route('/login.html')
+@app.route('/login')
 def root():
     # use render_template to convert the template code to HTML.
     # this function will look in the templates/ folder for your file.
-    return flask.render_template('index.html', page_title='Main Page')
+    return flask.render_template('login.html', state="null", page_title='Main Page')
 
+@app.route('/signedUp', methods=['POST', 'GET'])
+def loginSignUp():
+    user = flask.request.form['username']
+    password = flask.request.form['password']
+    if user and password:
+        add = f.add_user(user, password)
+        # if username exists already, display an error that an account already exists with username
+        if (add != True):
+            return flask.redirect(url_for('root', state="userExists"))
+    return flask.redirect(url_for('root', state="created"))
+
+@app.route('/signedIn', methods=['POST', 'GET'])
+def loginSignIn():
+    user = flask.request.form['usernameSignIn']
+    password = flask.request.form['passwordSignIn']
+    if user and password:
+        # if the username and password match what is in the database, then go to the index page
+        if (f.credentials(user, password)):
+            return flask.render_template('index.html', userID=f.getID(user))
+    # display an error that either the username or password is incorrect
+    return flask.redirect(url_for('root', state="wrongCredentials"))
 
 # note in our previous example we used separate functions for each template.
 # we can use our parameterization here to apply templates for many requests.
-@app.route('/p/<requested_page>')
+@app.route('/<requested_page>')
 def templater(requested_page):
     return flask.render_template(requested_page)
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+
